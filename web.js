349f8715382bc9c87053535ab789cb21baabc8c4
Duy Le <baduykt@yahoo.com>
2013-07-10 09:19:46 +0000

    Modify web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 
-var fs = require("fs");
-var content = fs.readFileSync("index.html");
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(content);
+   var content = fs.readFileSync('index.html'); 
+   response.send(content);
 }
 
 var port = process.env.PORT || 5000;
