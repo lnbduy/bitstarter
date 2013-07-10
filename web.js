958fea9b2b326e57c6a767d1f077b903f4b6c40b
Duy Le <baduykt@yahoo.com>
2013-07-10 09:15:22 +0000

    Modify web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
-var fs=require("fs");
-var content=fs.readFileSync("index.html");
+var fs = require("fs");
+var content = fs.readFileSync("index.html");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
